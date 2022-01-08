1bd615052d8b782517695b43c1e367246b67d5f8
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-08 20:14:19 +0300

    Write a program that summarizes integers. The program inputs integers in the following format: the first line contains an integer T - the number of tests, followed by T lines, each line contains two integers a_i and b_i separated by a space. The output for each of the T lines is the sum of a_i + b_i in the order in which the pairs come to the input.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,1 +1,21 @@
-#define MAX(a,b,r) do{int n1 = a;	int n2 = b;	r = n1 > n2 ? n1 : n2; break;} while(true)
+#include <iostream>
+#include <vector> //для массива vector
+#include <limits> //для работы с буфером https://ru.stackoverflow.com/questions/562229/%D0%9E%D1%87%D0%B8%D1%81%D1%82%D0%BA%D0%B0-%D0%B1%D1%83%D1%84%D0%B5%D1%80%D0%B0-%D0%B2%D0%B2%D0%BE%D0%B4%D0%B0-cin
+
+using std::endl;
+using std::cin;
+using std::cout;
+
+int main()
+{
+	int size = 0;
+	cin >> size;
+	int num1 = 0;
+	int num2 = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		cin >> num1 >> num2;
+		cout << num1 + num2 << endl;
+	}
+	return 0;
+}
