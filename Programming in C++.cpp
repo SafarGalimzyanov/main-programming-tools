6b3ab1c576e88173461598ecc9eb74ea9a6ab7d5
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-09 12:05:27 +0300

    Implement a swap_min function that takes a two-dimensional array of integers as input, looks for the string containing the smallest value among all elements of the array, and swaps the string with the first string of the array.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,24 +1,30 @@
 #include <cstdlib>
 
-int** transpose(const int* const* m, unsigned rows, unsigned cols)
+void swap_min(int* m[], unsigned rows, unsigned cols)
 {
-	//создание массива
-	int** mt = new int* [cols];
-	mt[0] = new int[cols * rows];
+    if (rows > 1) //если строка не одна, то ведётся поиск
+    {
+        //инициализация минимального числа и строки, на к-ой оно расположено
+        size_t min = m[0][0];
+        size_t RowToSwap = 0;
 
-	for (size_t i = 1; i != cols; ++i)
-	{
-		mt[i] = mt[i - 1] + rows;
-	}
+        //поиск объекта с минимальным значением и его строки
+        for (size_t i = 0; i < rows; ++i)
+        {
+            for (size_t j = 0; j < cols; ++j)
+            {
+                if (m[i][j] < min)
+                {
+                    min = m[i][j];
+                    RowToSwap = i;
+                }
+            }
+        }
 
-	//копирование значений из заданной матрицы в траспонированную
-	for (size_t i = 0; i < rows; ++i)
-	{
-		for (size_t j = 0; j < cols; ++j)
-		{
-			mt[j][i] = m[i][j];
-		}
-	}
-
-	return mt;
+        //смена указателей
+        int* temp = m[RowToSwap];
+        m[RowToSwap] = m[0]; //Указатель на строку RowToSwap теперь указывает на нулевую строку
+        m[0] = temp; //Указатель на нулевую строку теперь указывает на строку RowToSwap
+    }
+    return;
 }
