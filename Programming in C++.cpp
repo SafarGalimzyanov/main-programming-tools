cc00a53e654e8efaa0676cea0ee39e074185831e
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-09 11:51:48 +0300

    Write a function to find the first occurrence of a pattern in the text. As the first parameter, the function takes the text (C-style string) in which to search for the pattern. As the second parameter it takes the pattern string (C-style string) that you want to find. The function returns the position of the first occurrence of the pattern string, if it is present in the string (remember that in C++ it is usual to count from 0), and -1 if there is no pattern in the text.
    Note that an empty pattern (a string of length 0) can be found anywhere in the text.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,13 +1,60 @@
-void strcat(char* to, const char* from)
+int strstr(const char* text, const char* pattern)
 {
-    //переход к концу строки to
-    while (*to != '\0')
-    {
-        ++to;
-    }
-    while (*to = *from)
-    {
-        ++to;
-        ++from;
-    }
+	unsigned i = 0, j = 0, k = 0, pervoeVhozhdenie = 0, textSize = 0, patternSize = 0;
+	for (; pattern[j] != 0; ++j)
+	{
+	}
+	patternSize = j;
+
+	j = 0;
+	if (patternSize == 0)
+		return 0;
+
+	for (; text[i] != 0; ++i)
+	{
+	}
+	textSize = i;
+
+	i = 0;
+	if (textSize == 0)
+		return -1;
+
+	if (textSize < patternSize)
+		return -1;
+
+
+	//Проверили пустые шаблон, текст, когда шаблон больше текста. Теперь рабкотаем лапоньками
+	//Основной цикл: ищем первое вхождение нулевого элемента шаблона в текст
+	for (i = 0; i != textSize; ++i) {
+
+		if (i == (textSize - patternSize + 1))
+		{
+			//Если прошли по всему тексту - размер шаблона + 1 (на всякий, потом надо убрать)
+			//и нигде не встретили нулевой элемент шаблона, значит его тут не будет
+			return -1;
+		}
+		if (text[i] == pattern[0])
+		{
+			//Пришли сюда, Если встретили нулевой элемент шаблона в тексте
+			pervoeVhozhdenie = i;
+			//Теперь смотрим, как долго шаблон встречается в тексте
+			for (j = 0; text[pervoeVhozhdenie + j] == pattern[j]; ++j)
+			{
+				//Ну ищем дальше по тексту, чо
+				if (j == patternSize - 1)
+				{
+					//Если прошли весь шаблон успешно, значит возвращаем первое вхождение
+					return pervoeVhozhdenie;
+				}
+				else
+				{
+					//Споткнулись об ошибку, начинаем ВЕСЬ цикл заново, т.к. нужно сдвинуть i на 1
+				}
+			}
+
+		}
+
+	}
+	return -1;
+
 }
