5df0ec8bfb042bfff225694d36c53191c197ae15
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-09 11:39:18 +0300

    You need to write a program that "flips" a sequence of positive integers. The input is a sequence of space-separated positive integers. The sequence ends with zero. It is required to output this sequence in reverse order. At the output the numbers must also be separated by spaces.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,24 +1,16 @@
-unsigned NOD(unsigned num1, unsigned num2) {
+#include <iostream>
 
-	if (num2 != 1 && num2 != 0) {
-		return NOD(num2, num1 % num2);
-	}
-	else if (num2 == 0)
-		return num1;
-	else
-		return 1;
-}
+using std::cin;
+using std::cout;
 
-unsigned gcd(unsigned num1, unsigned num2) {
-	if (num1 < num2) {
-		if (num1 == 0)
-			return num2;
-
-		else return NOD(num2, num1);
+int main()
+{
+	int readNum = 0;
+	cin >> readNum;
+	if (readNum != 0)
+	{
+		main();
+		cout << readNum << " ";
 	}
-	else {
-		if (num2 == 0)
-			return num1;
-		else return NOD(num1, num2);
-	}
+	return 0;
 }
