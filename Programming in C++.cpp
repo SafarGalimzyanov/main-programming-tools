dacb31e6738a2f0b1577fdaf602854df95261049
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-09 12:44:44 +0300

    Implement several functions that give full access to the fields (see code template), even though they are private.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,93 +1,24 @@
-#include <cstddef> // size_t
-#include <cstring> // strlen, strcpy
+/*
+  struct Cls {
+  Cls(char c, double d, int i);
+  private:
+      char c;
+      double d;
+      int i;
+  };
+*/
 
-struct String {
+//Доступ к полю c объекта cls.
+char& get_c(Cls& cls) {
+    return *(char*)&cls;
+}
 
-    //сохранение длины строки в size и указателя на строку в str
-    String(const char* str = "")
-    {
-        this->size = strlen(str);
-        this->str = new char[size + 1];
-        strcpy(this->str, str);
-    }
+//Доступ к полю d объекта cls.
+double& get_d(Cls& cls) {
+    return *((double*)&cls + 1);
+}
 
-    //создание строки заданной длины, заполненной заданным символом
-    String(size_t n, char c)
-    {
-        this->size = n;
-        this->str = new char[size + 1];
-        this->str[n] = '\0';
-        while (n--)
-        {
-            this->str[n] = c;
-        }
-    }
-
-    //деструктор
-    ~String()
-    {
-        delete[] this->str;
-    }
-
-    //конструктор копирования
-    String(const String& other) : size(other.size), str(new char[other.size + 1])
-    {
-        for (size_t i = 0; i != this->size + 1; ++i)
-        {
-            this->str[i] = other.str[i];
-        }
-    }
-
-    //конструктор присваивания
-    String& operator=(const String& other)
-    {
-        if (this != &other)
-        {
-            String(other).swap(*this);
-            return *this;
-        }
-    }
-    //обмен значениями полей
-    void swap(String& other)
-    {
-        std::swap(this->size, other.size);
-        std::swap(this->str, other.str);
-    }
-
-    //добавление копии строки-аргумента в конец текущей строки
-    void append(String& other)
-    {
-        //суммарный размер двух строк
-        size_t sumSize = this->size + other.size;
-
-        //суммарная строка
-        char* sumStr = new char[sumSize + 1];
-
-        //копирование первой строки
-        for (size_t i = 0; i < this->size; i++)
-        {
-            sumStr[i] = this->str[i];
-        }
-
-        //копирование второй строки
-        for (size_t i = this->size, j = 0; i < sumSize; ++i, ++j)
-        {
-            sumStr[i] = other.str[j];
-        }
-
-        //добавление нулевого символа
-        sumStr[sumSize] = '\0';
-
-        //удаление скопированной строки
-        delete[] this->str;
-
-        //установка указателя на суммарную строку
-        this->str = sumStr;
-
-        //запоминание суммарного размера
-        this->size += other.size;
-    }
-
-    size_t size;
-    char* str;
-};
+//Доступ к полю i объекта cls.
+int& get_i(Cls& cls) {
+    return *((int*)&cls + 4);
+}
