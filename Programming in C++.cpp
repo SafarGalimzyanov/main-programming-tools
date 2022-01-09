ea3f56d00b4b887a58a85cc407b7cb58d550c3eb
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-09 13:01:13 +0300

    You need to implement a function that takes as input two pointers to the base class Expression, and returns true if both pointers actually point to objects of the same class, and false otherwise.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,55 +1,11 @@
-struct Expression
+bool check_equals(Expression const* left, Expression const* right)
 {
-    virtual double evaluate() const = 0; //чистый виртуальный метод
-    virtual ~Expression() {}
-};
-
-struct Number : Expression
-{
-    Number(double value)
-        : value(value)
-    {}
-    double evaluate() const override
+    if (*(void**)(left) == *(void**)(right))
     {
-        return this->value;
+        return true;
     }
-    //~Number(){}
-private:
-    double value;
-};
-
-struct BinaryOperation : Expression
-{
-    BinaryOperation(Expression const* left, char op, Expression const* right)
-        : left(left), op(op), right(right) //op - это операция
-    { }
-
-    double evaluate() const override
+    else
     {
-        if (op == '+')
-        {
-            return left->evaluate() + right->evaluate();
-        }
-        if (op == '-')
-        {
-            return left->evaluate() - right->evaluate();
-        }
-        if (op == '*')
-        {
-            return left->evaluate() * right->evaluate();
-        }
-        if (op == '/')
-        {
-            return left->evaluate() / right->evaluate();
-        }
+        return false;
     }
-    ~BinaryOperation() {
-        delete left;
-        delete right;
-    }
-
-private:
-    Expression const* left;
-    Expression const* right;
-    char op;
-};
+}
