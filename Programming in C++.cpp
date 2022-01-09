32454b26b2e1091bcb5a58b67f4c63ae9b821eaa
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-09 14:09:10 +0300

    As an exercise in partial specialization of class templates, you are encouraged to implement a simple SameType template. This template does not contain any methods, but only one static constant field of type bool, named value. The template accepts two type parameters, and if the two type parameters of the template are the same type, the static value field must store the value true, otherwise the value false.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,23 +1,11 @@
-#include <cstddef>
-
-// Параметры функции copy_n идут в следующем
-// порядке:
-//   1. целевой массив
-//   2. массив источник
-//   3. количество элементов, которые нужно
-//      скопировать
-//
-// Вам нужно реализовать только функцию copy_n,
-// чтобы ее можно было вызвать так, как показано
-// в примере.
-
-// put your code here
-template <typename Type1, typename Type2>
-void copy_n(Type1* array1, Type2* array2, size_t s)
+template<typename mue, typename shue>
+struct SameType
 {
-    for (int i = 0; i != s; ++i)
-    {
-        array1[i] = Type1(array2[i]);
-    }
-    return;
-}
+    static const bool value = 0;
+};
+
+template <typename mue>
+struct SameType <mue, mue>
+{
+    static const bool value = 1;
+};
