bf4ad46803b0c8d5307c9d2522f92aae8dda5ed9
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-09 13:48:13 +0300

    You are given a Rational class that describes a rational number. It has add, sub, mul, and div methods that add to it, subtract from it, multiply by it, and divide by it, respectively. It also has a method neg, which reverses the sign.
    You need to define +=, -=, *=, /= operators for the Rational class so that they can take both objects of type Rational and integers as arguments. You also need to define unary minus and plus operators for the Rational class.
    
    Define addition, subtraction, multiplication and division operators for the Rational class so that objects of Rational type can be added (subtracted, multiplied and divided) not only with each other but also with integers.
    
    Implement operators <, <=, >, >=, ==, != for Rational class so that it is possible to compare Rational class objects not only with each other but also with integers.
    
    Add a cast to double operator to the Rational class.

--- Programming in C++.cpp
+++ Programming in C++.cpp
@@ -1,38 +1,120 @@
-struct ScopedPtr
+struct Rational
 {
-    //создание экземпл€ра класса
-    explicit ScopedPtr(Expression* ptr = 0) : ptr_(ptr) {}
-    //деструктор
-    ~ScopedPtr()
-    {
-        delete this->ptr_;
-    }
-    //вывод указател€
-    Expression* get() const
-    {
-        return this->ptr_;
-    }
-    //создаЄтс€ новый указатель, старый обнул€етс€
-    Expression* release()
-    {
-        Expression* aue = this->ptr_;
-        this->ptr_ = NULL;
-        return aue;
-    }
-    //удаление старого указател€, установка на новый
-    void reset(Expression* ptr = 0)
-    {
-        delete this->ptr_;
-        this->ptr_ = ptr;
-    }
-    Expression& operator*() const {};
-    Expression* operator->() const {};
+    Rational(int numerator = 0, int denominator = 1);
 
+    void add(Rational rational);
+    void sub(Rational rational);
+    void mul(Rational rational);
+    void div(Rational rational);
+
+    void neg();
+    void inv();
+
+    //операторы +=, -=, *=, /= дл€ объектов класса Rational и целых чисел
+    Rational& operator+=(Rational const& rational) {
+        this->add(rational);
+        return *this;
+    }
+    Rational& operator+=(double i) {
+        this->add(Rational(i));
+        return *this;
+    }
+    Rational& operator-=(Rational const& rational) {
+        this->sub(rational);
+        return *this;
+    }
+    Rational& operator-=(double i) {
+        this->sub(Rational(i));
+        return *this;
+    }
+    Rational& operator*=(Rational const& rational) {
+        this->mul(rational);
+        return *this;
+    }
+    Rational& operator*=(double i) {
+        this->mul(Rational(i));
+        return *this;
+    }
+    Rational& operator/=(Rational const& rational) {
+        this->div(rational);
+        return *this;
+    }
+    Rational& operator/=(double i) {
+        this->div(Rational(i));
+        return *this;
+    }
+    Rational operator-() const {
+        Rational temp(this->numerator_, this->denominator_);
+        temp.neg();
+        return temp;
+    }
+    Rational operator+() const {
+        Rational temp(this->numerator_, this->denominator_);
+        return temp;
+    }
+
+    double to_double() const;
+
+    //оператор приведени€ к double
+    operator double() const {
+        return double(this->numerator_) / (double(this->denominator_));
+    }
+
+    friend bool operator==(Rational const& left, Rational const& right);
+    friend bool operator!=(Rational const& left, Rational const& right);
+    friend bool operator<(Rational const& left, Rational const& right);
+    friend bool operator<=(Rational const& left, Rational const& right);
+    friend bool operator>(Rational const& left, Rational const& right);
+    friend bool operator>=(Rational const& left, Rational const& right);
 
 private:
-    // запрещаем копирование ScopedPtr
-    ScopedPtr(const ScopedPtr&);
-    ScopedPtr& operator=(const ScopedPtr&);
-
-    Expression* ptr_;
+    int numerator_;
+    int denominator_;
 };
+
+//операторы сравнени€ дл€ объектов класса Rational и целых чисел
+bool operator==(Rational const& left, Rational const& right)
+{
+    return (left.numerator_ * right.denominator_) == (left.denominator_ * right.numerator_);
+}
+bool operator!=(Rational const& left, Rational const& right)
+{
+    return !(left == right);
+}
+
+bool operator<(Rational const& left, Rational const& right)
+{
+    return (left.numerator_ * right.denominator_) < (left.denominator_ * right.numerator_);
+}
+bool operator<=(Rational const& left, Rational const& right)
+{
+    return (left < right) || (left == right);
+}
+
+bool operator>(Rational const& left, Rational const& right)
+{
+    return (left.numerator_ * right.denominator_) > (left.denominator_ * right.numerator_);
+}
+
+bool operator>=(Rational const& left, Rational const& right)
+{
+    return (left > right) || (left == right);
+}
+
+//операторы +, -, *, /   дл€ объектов класса Rational и целых чисел
+Rational operator+(Rational rational1, Rational rational2) {
+    rational1.add(rational2);
+    return rational1;
+}
+Rational operator-(Rational rational1, Rational rational2) {
+    rational1.sub(rational2);
+    return rational1;
+}
+Rational operator*(Rational rational1, Rational rational2) {
+    rational1.mul(rational2);
+    return rational1;
+}
+Rational operator/(Rational rational1, Rational rational2) {
+    rational1.div(rational2);
+    return rational1;
+}
